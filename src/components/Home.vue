75bc56e24a59abdad520a68ad7e9ca535deb2b2e
Butov Konstantin <thekillswitchru@gmail.com>
2019-04-14 01:32:58 +0600

    Progress save

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="home">Home page</div>
+  <div class="home">
+    <Header/>
+    <div class="home-content">Home page</div>
+    <Footer/>
+  </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 export default {
-  data(){
-    return {
-
-    }
+  name: 'home',
+  components: {
+    Header,
+    Footer
   }
 }
 </script>
 
 <style scoped>
-.home{
+.home-content{
   height: 1000px;
-  margin: -100px 30px 0;
+  margin: -40vh 30px 0;
   border-radius: 6px;
   box-shadow:
   0 16px 24px 2px rgba(0,0,0,.14),
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import About from '@/components/About'
-import Login from '@/components/Login'
-import Register from '@/components/Register'
+import NotFound from '@/components/NotFound'
 
 Vue.use(Router)
 
@@ -11,8 +9,7 @@ export default new Router({
   mode: 'history',
   routes: [
     {path: '/', name: 'Home', component: Home},
-    {path: '/about', name: 'About', component: About},
-    {path: '/login', name: 'Login', component: Login},
-    {path: '/register', name: 'Register', component: Register}
+    {path: '/void', name: 'void', component: NotFound},
+    {path: '*', redirect: '/void'}
   ]
 })
