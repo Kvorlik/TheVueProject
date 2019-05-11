1bcea1997fa138ed0cb68544a5e197b5a244295c
Butov Konstantin <thekillswitchru@gmail.com>
2019-05-12 02:26:31 +0600

    Added 'back' buttons on Sveta's pages

--- src/components/Begin.vue
+++ src/components/Begin.vue
@@ -26,12 +26,21 @@
         <span>фитнес-трекер</span>
         для контроля изменения состояния своего организма в процессе тренировок.
       </p>
+      <div class="flex">
+        <div class="link-button shadow" @click="goBack">Назад</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'begin',
+  methods: {
+    goBack() {
+      this.$router.go(-1)
+    }
+  }
 }
 </script>
 
@@ -73,6 +82,27 @@ img {
   transition: 0.2s;
 }
 
+.link-button:hover{
+  cursor: pointer;
+  box-shadow:
+  0 6px 6px 0 rgba(60,64,67,.2),
+  0 6px 6px 6px rgba(60,64,67,.2);
+  color: black;
+  transform: translateY(-5px);
+}
+
+.link-button{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #ff9900;
+  width: 200px;
+  height: 50px;
+  font-size: 18px;
+  border-radius: 6px;
+  color: white;
+}
+
 .p{
   margin: 0 50px 0;
   padding: 30px 0 30px;
@@ -86,7 +116,15 @@ img {
 }
 
 .content-area{
+  min-height: 90vh;
+  position: relative;
   margin: 0 50px 0;
   padding: 30px 0 30px;
+}
+
+.flex{
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
