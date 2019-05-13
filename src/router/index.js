ab8f7ffe8943a1d9d3dc15c6381cb4c50da94656
Butov Konstantin <thekillswitchru@gmail.com>
2019-05-13 23:34:43 +0600

    Added /training page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Void from '@/components/Void'
 import Test from '@/components/Test'
 import Begin from '@/components/Begin'
 import Need from '@/components/Need'
+import Training from '@/components/Training'
 
 Vue.use(Router)
 
@@ -16,6 +17,7 @@ export default new Router({
     {path: '/test', name: 'Test', component: Test},
     {path: '/where-to-begin', name: 'Begin', component: Begin},
     {path: '/need-to-know', name: 'Need', component: Need},
+    {path: '/training', name: 'Training', component: Training},
     {path: '*', redirect: '/void'}
   ]
 })
