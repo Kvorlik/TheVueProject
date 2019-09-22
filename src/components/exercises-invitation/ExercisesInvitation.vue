04af6e4bb97320a9556a272e03074390084d40bc
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:20:21 +0600

    Add exercises invitation component

---
+++ src/components/exercises-invitation/ExercisesInvitation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="test-invitation flex">
+    <slot/>
+    <regular-button
+      :href="href"
+      :width="450"
+      :height="90"
+      :font-size="25"
+    >
+      {{ buttonText }}
+    </regular-button>
+  </div>
+</template>
+
+<script>
+import { RegularButton } from '@/components';
+
+export default {
+  name: 'ExercisesInvitation',
+  props: {
+    buttonText: String,
+    href: String
+  },
+  components: {
+    RegularButton
+  }
+}
+</script>
+
+<style scoped lang="css" src="./ExercisesInvitation.css"/>
