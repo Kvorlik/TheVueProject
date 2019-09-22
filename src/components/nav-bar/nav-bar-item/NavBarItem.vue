73a6b3391eea85105bf1758cccf740181175d548
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:24:15 +0600

    Add nav bar item component

---
+++ src/components/nav-bar/nav-bar-item/NavBarItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="nav-item underline">
+    <router-link :to="href">
+      <span><slot/></span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBarItem',
+  props: {
+    href: String
+  }
+}
+</script>
+
+<style scoped lang="css" src="./NavBarItem.css"/>
