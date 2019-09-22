cc080f244cd43016af1b6cdee2ce145ade70d139
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:32:23 +0600

    Add common layout

---
+++ src/views/layouts/common-layout/CommonLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="common-layout">
+        <full-screen-background :background="this.background"/>
+        <div class="content-area">
+          <nav-bar/>
+          <main-titles v-if="showMainTitles"/>
+          <main class="content">
+            <slot/>
+          </main>
+        </div>
+        <common-footer/>
+    </div>
+</template>
+
+<script>
+import { FullScreenBackground, NavBar, CommonFooter, MainTitles } from '@/components';
+export default {
+    name: 'CommonLayout',
+    props: {
+      showMainTitles: Boolean,
+      background: String
+    },
+    components: {
+      FullScreenBackground,
+      NavBar,
+      CommonFooter,
+      MainTitles
+    }
+}
+</script>
+
+<style scoped lang="css" src="./CommonLayout.css"/>
