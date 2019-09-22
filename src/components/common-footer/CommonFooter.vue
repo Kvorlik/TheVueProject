6bb9dd341558b0a9460b7e1b731e8e868758caa9
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:19:40 +0600

    Add common footer component

---
+++ src/components/common-footer/CommonFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <footer>
+    <div class="container">
+      <div>
+        &#169; 2019, made by dream team for OmSU
+      </div>
+
+      <div class="links">
+        <square-logo href="https://github.com/Kvorlik/sportify" :src="github"/>
+        <square-logo href="https://ru.vuejs.org/index.html" :src="vue"/>
+        <square-logo href="https://firebase.google.com/" :src="firebase"/>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+import SquareLogo from '../square-logo/SquareLogo';
+import { GithubLogo, VueLogo, FirebaseLogo } from './images'
+
+export default {
+  name: 'CommonFooter',
+  components: {
+    SquareLogo
+  },
+  data() {
+    return {
+      github: GithubLogo,
+      vue: VueLogo,
+      firebase: FirebaseLogo
+    }
+  }
+}
+</script>
+
+<style scoped lang="css" src="./CommonFooter.css"/>
---
+++ src/components/common-footer/images/index.js
@@ -0,0 +1,3 @@
+export { default as GithubLogo } from './github-logo.png';
+export { default as VueLogo } from './vue-logo.png';
+export { default as FirebaseLogo } from './firebase-logo.png';
