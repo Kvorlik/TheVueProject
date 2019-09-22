3cd1800f0329e7362bbeb7c09a25e9cf188a4462
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:20:55 +0600

    Add full screen background component

---
+++ src/components/full-screen-background/FullScreenBackground.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="background" :style="getBackground"/>
+</template>
+
+<script>
+export default {
+  name: 'FullScreenBackground',
+  props: {
+    background: String
+  },
+  computed: {
+    getBackground() {
+      return 'backgroundImage: url(' + this.background + ')';
+    }
+  }
+}
+</script>
+
+<style scoped lang="css" src="./FullScreenBackground.css">
