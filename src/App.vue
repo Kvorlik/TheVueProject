b5b4df8bc2b6b93f8569033fc442578f398b6468
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:14:33 +0600

    Remove styles

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,11 @@
 <template>
   <div id="app">
-    <Header/>
     <router-view/>
-    <Footer/>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
 export default {
   name: 'app',
-  components: {
-    Header,
-    Footer
-  }
 }
 </script>
-
-<style>
-@import url('https://fonts.googleapis.com/css?family=Exo+2');
-body {
-  margin: 0;
-  padding: 0;
-  background-color: #eeeeee;
-}
-
-* {
-  font-family: 'Exo 2', sans-serif;
-}
-
-*::selection {
-  background: transparent;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-}
-
-a {
-  text-decoration: none;
-  outline: 0;
-  color: white;
-}
-
-.swal-button--confirm{
-  background-color: #ff9900;
-}
-
-.swal-button--confirm:not([disabled]):hover{
-  background-color: #ff9900;
-  color: black;
-}
-
-.swal-text{
-  font-size: 20px;
-}
-</style>
