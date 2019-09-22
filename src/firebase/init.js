da6e505c036b22debc3be960e526fbeab45bbba4
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:50:54 +0600

    Returned firebase config back for a while

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,14 +2,13 @@ import firebase from "firebase/app";
 import firestore from "firebase/firestore";
 
 // Initialize Firebase
-// Values for author's database removed due to security reasons.
 var config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyChTv2Ncw4CxC2yhvMqGrc-iBQVN1YyJY0",
+  authDomain: "vue-omsu-6aa01.firebaseapp.com",
+  databaseURL: "https://vue-omsu-6aa01.firebaseio.com",
+  projectId: "vue-omsu-6aa01",
+  storageBucket: "vue-omsu-6aa01.appspot.com",
+  messagingSenderId: "953452980835"
 };
 const firebaseApp = firebase.initializeApp(config);
 
