3632814e160e169b5f0f365c22f6b3a78670e459
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:17:41 +0600

    Add circle button component

---
+++ src/components/circle-button/CircleButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-link :to="href">
+    <div class="circle-link-button flex shadow" :style="getBackground">
+      <div class="button-text underline">
+        <span>
+          <slot/>
+        </span>
+      </div>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'CircleButton',
+  props: {
+    href: String,
+    background: String
+  },
+  computed: {
+    getBackground() {
+      return 'backgroundImage: url(' + this.background + ')';
+    }
+  }
+}
+</script>
+
+<style scoped lang="css" src="./CircleButton.css"/>
