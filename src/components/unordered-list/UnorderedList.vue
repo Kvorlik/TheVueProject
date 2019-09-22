40144b2e39bbecf2053ae16c79a656e9f2be5ec1
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:28:39 +0600

    Add unordered list component

---
+++ src/components/unordered-list/UnorderedList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul :class="{
+    positive: marker.positive,
+    negative: marker.negative
+  }">
+    <slot/>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'PositiveList',
+  props: {
+    marker: Object
+  }
+}
+</script>
+
+<style scoped lang="css" src="./UnorderedList.css"/>
