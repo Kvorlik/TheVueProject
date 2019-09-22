8be0356bc18127e64f084157f3ec16451fb38847
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:23:00 +0600

    Add main titles component

---
+++ src/components/main-titles/MainTitles.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="titles">
+    <div class="headertitle">Sportify</div>
+    <div class="headersubtitle">&laquo;Спорт&nbsp;&mdash; это жизнь&raquo;</div>
+  </div>
+</template>
+
+<style scoped lang="css" src="./MainTitles.css"/>
