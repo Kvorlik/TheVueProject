71a7c1461c5b655c5e7954509617e5a5e3330ab2
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:24:58 +0600

    Add regular button

---
+++ src/components/regular-button/RegularButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <router-link :to="href">
+      <button class="link-button" :style="getStyles">
+        <slot/>
+      </button>
+    </router-link>
+</template>
+
+<script>
+export default {
+  name: 'RegularButton',
+  props: {
+    href: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: Number,
+      default: 200
+    },
+    height: {
+      type: Number,
+      default: 50
+    },
+    fontSize: {
+      type: Number,
+      default: 18
+    }
+  },
+  computed: {
+    getStyles() {
+      return 'width: ' + this.width + 'px; height: ' + this.height + 'px; font-size: ' + this.fontSize + 'px;'
+    }
+  }
+}
+</script>
+
+<style scoped lang="css" src="./RegularButton.css"/>
