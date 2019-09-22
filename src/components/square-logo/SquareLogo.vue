53fd49405d5958cdae854b93ea06798f466f4274
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:26:53 +0600

    Add square logo component

---
+++ src/components/square-logo/SquareLogo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="logo">
+    <a :href="href" target="_blank">
+      <img :src="src"/>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SquareLogo',
+  props: {
+    href: String,
+    src: String
+  }
+}
+</script>
+
+<style scoped>
+  .logo img {
+    width: 32px;
+    margin: 10px 10px 0;
+  }
+</style>
