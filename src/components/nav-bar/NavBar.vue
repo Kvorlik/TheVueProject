55a9c052719aeb14d1f3279b6beca1c0f8eb8c76
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:23:40 +0600

    Add nav bar component

---
+++ src/components/nav-bar/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar">
+    <main-logo id="first"/>
+    <spacer/>
+    <nav-bar-item :href="routes.ABOUT" id="second">О нас</nav-bar-item>
+    <nav-bar-item :href="routes.REGISTER" id="third">Регистрация</nav-bar-item>
+  </nav>
+</template>
+
+<script>
+import { ROUTES } from "@/common/constants";
+import MainLogo from '../main-logo/MainLogo';
+import NavBarItem from '../nav-bar/nav-bar-item/NavBarItem';
+import Spacer from '../spacer/Spacer';
+
+export default {
+  name: 'NavBar',
+  components: {
+    MainLogo,
+    NavBarItem,
+    Spacer
+  },
+  computed: {
+    routes() {
+      return { ...ROUTES };
+    }
+  }
+}
+</script>
+
+<style scoped lang="css" src="./NavBar.css"/>
