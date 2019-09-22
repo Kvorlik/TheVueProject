a2344fc2a715d28d9cd05f38e3f6c6c15c1e4d93
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:32:57 +0600

    Add void layout

---
+++ src/views/layouts/void-layout/VoidLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="void-layout">
+        <full-screen-background :background="background"/>
+        <div class="content-area">
+          <nav-bar/>
+          <main class="content">
+            <slot/>
+          </main>
+        </div>
+        <common-footer/>
+    </div>
+</template>
+
+<script>
+import { FullScreenBackground, NavBar, CommonFooter } from '@/components';
+export default {
+    name: 'VoidLayout',
+    props: {
+      background: String
+    },
+    components: {
+      FullScreenBackground,
+      NavBar,
+      CommonFooter
+    }
+}
+</script>
+
+<style scoped lang="css" src="./VoidLayout.css"/>
