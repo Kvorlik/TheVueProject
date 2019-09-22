ec9de589e440245adbf5bc9e52180d68fcfbacc4
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:22:30 +0600

    Add main logo component

---
+++ src/components/main-logo/MainLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="logo">
+    <router-link :to='home'>
+      <img src="./main-logo.jpg" alt="Logo">
+    </router-link>
+  </div>
+</template>
+
+<script>
+import { ROUTES } from "@/common/constants";
+export default {
+  name: 'MainLogo',
+  computed: {
+    home() {
+      return ROUTES.HOME;
+    }
+  }
+}
+</script>
+
+<style scoped lang="css" src="./MainLogo.css"/>
