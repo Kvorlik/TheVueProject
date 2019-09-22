a0caa220b443a537987c754df366ac17654c2180
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:33:40 +0600

    Add test layout

---
+++ src/views/layouts/test-layout/TestLayout.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="test-layout">
+        <full-screen-background :background="background"/>
+        <div class="content-area">
+          <nav-bar/>
+          <main
+            class="content"
+            :class="{
+              'wrist-background': contentBackground.wristBackground,
+              'forest-background': contentBackground.forestBackground
+            }"
+          >
+            <slot/>
+          </main>
+        </div>
+        <common-footer/>
+    </div>
+</template>
+
+<script>
+import { FullScreenBackground, NavBar, CommonFooter } from '@/components';
+export default {
+    name: 'TestLayout',
+    props: {
+      background: String,
+      contentBackground: Object
+    },
+    components: {
+      FullScreenBackground,
+      NavBar,
+      CommonFooter
+    }
+}
+</script>
+
+<style scoped lang="css" src="./TestLayout.css"/>
