2304ad672a005a46c4c2ac455014c4c476ae5ad4
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:43:40 +0600

    Add index for views

---
+++ src/views/index.js
@@ -0,0 +1,13 @@
+export { default as CommonLayout } from './layouts/common-layout/CommonLayout';
+export { default as VoidLayout } from './layouts/void-layout/VoidLayout';
+export { default as TestLayout } from './layouts/test-layout/TestLayout';
+
+export { default as HomePage } from './home/HomePage';
+export { default as VoidPage } from './void/VoidPage';
+export { default as BeginPage } from './begin/BeginPage';
+export { default as NeedToKnowPage } from './need/NeedToKnowPage';
+export { default as StatisticsPage } from './statistics/StatisticsPage';
+export { default as ExercisesPage } from './exercises/ExercisesPage';
+export { default as TestPage } from './test/TestPage';
+
+export { default as steps } from './test/steps';
