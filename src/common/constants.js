369d87cbb725f4b08b33290e1bbde763ad753d8b
Butov Konstantin <thekillswitchru@gmail.com>
2019-09-22 19:14:46 +0600

    Add constants

---
+++ src/common/constants.js
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  HOME: '/',
+  ABOUT: '/about',
+  REGISTER: '/register',
+  WHERE_TO_BEGIN: '/where-to-begin',
+  NEED_TO_KNOW: '/need-to-know',
+  STATISTICS: '/statistic',
+  EXERCISES: '/exercises',
+  TEST: '/test',
+  VOID: '/void'
+};
+
+export default {
+    ROUTES
+};
